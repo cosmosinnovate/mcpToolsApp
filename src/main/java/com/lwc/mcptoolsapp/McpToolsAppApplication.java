f41e00325eb011380cd5bacd5478d3b904c80393
Taban Cosmos <cosmostaban@gmail.com>
2025-05-28 18:51:40 -0700

    initial mcp server app

---
+++ src/main/java/com/lwc/mcptoolsapp/McpToolsAppApplication.java
@@ -0,0 +1,13 @@
+package com.lwc.mcptoolsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McpToolsAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(McpToolsAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lwc/mcptoolsapp/McpToolsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lwc.mcptoolsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McpToolsAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
